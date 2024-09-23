e3a7c7823f56235171a9579c7dfb751bb46b8f6b
sugeetha-sugee <146417443+sugeetha-sugee@users.noreply.github.com>
2024-09-23 16:05:02 +0530

    Add files via upload

---
+++ CompanyDetails/src/org/company/CompanyInfo.java
@@ -0,0 +1,20 @@
+package org.company;
+
+public class CompanyInfo {
+   private void companyId() {
+	   System.out.println("E21CS045");
+   }
+   private void companyAddress() {
+	   System.out.println("Bangalore");
+   }
+   private void CompanyName() {
+	   System.out.println("Deloitte");
+   }
+   public static void main(String[] args) {
+	CompanyInfo b = new CompanyInfo();
+	b.companyId();
+	b.CompanyName();  
+	b.companyAddress();
+	
+}
+}
