cbe845a1344117cabc43cda10c946057d9ce25e9
sugeetha-sugee <146417443+sugeetha-sugee@users.noreply.github.com>
2024-09-23 16:05:44 +0530

    Add files via upload

---
+++ DataTypes/src/org/simple/EmployeeDetails.java
@@ -0,0 +1,27 @@
+package org.simple;
+
+public class EmployeeDetails {
+       public static void main(String[] args) {
+		  String name =  "Krish";
+		  byte age = 22;
+		  long mobile = 8977653445l;
+		  short salary = 30000;
+		  char gender = 'M';
+		  float f = 3.4f;
+		  double d = 45.97878487976;
+		  int acc = 465358938;
+		  boolean bool = true;
+		  System.out.println("Employee name is: "+name+"\n"+
+		                      "Employee age is: "+age+"\n"+
+		                      "Employee number is: "+mobile+"\n"+
+		                        "Employee salary is: "+salary+"\n"+
+		                      "Employee gender is: "+gender+"\n"+
+		                      "Employee f is: "+f+"\n"+
+		                      "Employee d is: "+d+"\n"+
+		                      "Employee account is: "+acc+"\n"+
+		                      "Employee bool is: "+bool); 
+				              
+		  
+		  
+	}
+}
