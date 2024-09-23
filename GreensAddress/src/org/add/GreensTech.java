a9125f1a18abba1842fbd256fe0f0eb74e531c17
sugeetha-sugee <146417443+sugeetha-sugee@users.noreply.github.com>
2024-09-23 16:07:08 +0530

    Add files via upload

---
+++ GreensAddress/src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensTech {
+     private void greensOmr() {
+    	 System.out.println("OMR");
+     }
+     private void greensAdayar() {
+    	 System.out.println("ADAYAR");
+     }
+     private void greensTambaram() {
+    	 System.out.println("TAMBARAM");
+     }
+     private void greensVelachery() {
+    	 System.out.println("VELACHERY");
+     }
+     private void greensAnnanagar() {
+    	 System.out.println("ANNANAGAR");
+     }
+     public static void main(String[] args) {
+		GreensTech a = new GreensTech();
+		a.greensOmr();
+		a.greensAdayar();
+		a.greensTambaram();
+		a.greensVelachery();
+		a.greensAnnanagar();
+	}
+}
